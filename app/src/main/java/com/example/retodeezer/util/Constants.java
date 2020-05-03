de0f13fd229e8943d5a020b44fab44605af452f6
Bryan Garcés <bragapo23@gmail.com>
2020-05-02 19:42:02 -0500

    delete of unused lines

--- app/src/main/java/com/example/retodeezer/util/Constants.java
+++ app/src/main/java/com/example/retodeezer/util/Constants.java
@@ -5,5 +5,4 @@ public class Constants {
     public final static int SEARCH_CALLBACK=1;
     public static final int TRACKLISTS_TAP = 2 ;
     public static final int LISTINFO =3 ;
-    public static final int LOAD_ALL_PLAYLISTS = 4;
 }
